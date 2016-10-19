9b4672da5be82c7b39a04cea6e4903ce6e5dc7d9
Manuel HUEZ <manuel.huez@gmail.com>
2016-10-19 15:24:38 -0400

    Improve comments in card.ts

--- src/processout/card.ts
+++ src/processout/card.ts
@@ -9,9 +9,21 @@ module ProcessOut {
      * Expiry class, mainly used in the Card class
      */
     export class Expiry {
+        /**
+         * Month is the expiry month
+         */
         protected month: number;
-        protected year:  number;
 
+        /**
+         * Year is the expiry year
+         */
+        protected year: number;
+
+        /**
+         * Constructor of the Expiry class. Accepts 2 or 4 digits year
+         * @param {number} month
+         * @param {number} year
+         */
         constructor(month: number, year: number) {
             this.month = month;
             this.year  = year;
@@ -21,6 +33,12 @@ module ProcessOut {
             }
         }
 
+        /**
+         * Parse parses a date string composed of the month and the year
+         * and returns an Expiry object
+         * @param {string} exp
+         * @return {Expiry}
+         */
         public static parse(exp: string): Expiry {
             var exps = exp.split(" / ");
             if (exps.length <= 1) {
@@ -33,18 +51,35 @@ module ProcessOut {
             return new Expiry(Number(exps[0]), Number(exps[1]));
         }
 
+        /**
+         * getMonth returns the expiry month of the card
+         * @return {number}
+         */
         public getMonth(): number {
             return this.month;
         }
 
+        /**
+         * getYear returns the expiry year of the card
+         * @return {number}
+         */
         public getYear(): number {
             return this.year;
         }
 
+        /**
+         * string converts the expiry object back to its string representation
+         * @return {string}
+         */
         public string(): string {
-            return this.month + "/" + this.year;
+            return this.month + " / " + this.year;
         }
 
+        /**
+         * Format formats an expiry date string
+         * @param {string} exp
+         * @return {string}
+         */
         public static format(exp: string): string {
             if (exp.length == 2)
                 return exp + " / ";
@@ -60,30 +95,59 @@ module ProcessOut {
      * Card class
      */
     export class Card {
+        /**
+         * Number is the card number
+         * @type {string}
+         */
         protected number: string;
-        protected expiry: Expiry;
-        protected cvc:    string;
 
+        /**
+         * Expiry is the expiry date of the card stored in an Expiry object
+         */
+        protected expiry: Expiry;
+
+        /**
+         * CVC is the CVC of the card
+         */
+        protected cvc: string;
+
+        /**
+         * Constructor of the card class
+         */
         constructor(number: string, expiry: Expiry, cvc: string) {
             this.number = number.replace(/ /gi, "").replace(/\-/gi, "").replace(/\-/gi, "");
             this.cvc    = cvc;
             this.expiry = expiry;
         }
 
+        /** 
+         * getNumber returns the card number trimmed from all spaces
+         * @return {string}
+         */
         public getNumber(): string {
             return this.number;
         }
 
+        /**
+         * getExpiry returns the expiry date of the card in the Expiry object
+         * @return {Expiry}
+         */
         public getExpiry(): Expiry {
             return this.expiry;
         }
 
+        /**
+         * getCVC returns the CVC of the card
+         * @return {string}
+         */
         public getCVC(): string {
             return this.cvc;
         }
 
         /**
          * formatNumber formats a card number
+         * @param {string} number
+         * @return {string}
          */
         public static formatNumber(number: string): string {
             var v       = number.replace(/\s+/g, "").replace(/[^0-9]/gi, "");
@@ -103,8 +167,10 @@ module ProcessOut {
 
         /**
          * luhn does a basic luhn check on the card number
+         * @param {number} cardNo
+         * @return {boolean}
          */
-        protected static luhn(cardNo) {
+        protected static luhn(cardNo): boolean {
             var s = 0;
             var doubleDigit = false;
             for (var i = cardNo.length - 1; i >= 0; i--) {
@@ -123,6 +189,8 @@ module ProcessOut {
 
         /**
          * setExpiry sets the expiry date of the card
+         * @param {Expiry} exp
+         * @return {void}
          */
         public setExpiry(exp: Expiry): void {
             this.expiry = exp;
@@ -130,6 +198,7 @@ module ProcessOut {
 
         /**
          * validate validates a credit card details
+         * @return {string}
          */
         public validate(): string {
             if (this.number.length != 16)
