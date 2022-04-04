d5373958f02b9ff3e8fa57879cd4bae29672efc8
Andrea Vasapollo <andrea.vasapollo@checkout.com>
2022-04-04 17:09:41 +0100

    chore: rename alternative

--- src/processout/applepay.d.ts
+++ src/processout/applepay.d.ts
@@ -13,7 +13,7 @@ interface ApplePayPaymentRequest {
   shippingType?: any;
   requiredBillingContactFields?: any;
   requiredShippingContactFields?: any;
-  certificateId: string;
+  alternativeCertificateId: string;
 }
 declare enum ApplePayStatusCodes {
 // The requested action succeeded.
