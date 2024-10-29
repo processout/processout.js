bd4adb0e7ca0ef8c80e0dc0903d242fe3c191deb
Jakub Jasiński <jakub.jasinski@processout.com>
2024-10-29 11:29:39 +0100

    after review

--- src/dynamic-checkout/utils/translations.ts
+++ src/dynamic-checkout/utils/translations.ts
@@ -1,21 +1,19 @@
 /// <reference path="../references.ts" />
 
 module ProcessOut {
+  const locales = {
+    en: en,
+    es: es,
+    fr: fr,
+    pl: pl,
+    pt: pt,
+  };
+
   export class Translations {
     static getText(key: string, locale: string) {
-      switch (locale) {
-        case "en":
-        default:
-          return en[key];
-        case "es":
-          return es[key];
-        case "fr":
-          return fr[key];
-        case "pl":
-          return pl[key];
-        case "pt":
-          return pt[key];
-      }
+      const keys = locales[locale] || locales.en;
+
+      return keys[key] || "";
     }
   }
 }
