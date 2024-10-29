f40b606eafdb38b9f42952f7bc062fa42d29abc0
Jakub Jasiński <jakub.jasinski@processout.com>
2024-10-29 11:44:26 +0100

    fix: fix dc locales

--- src/dynamic-checkout/utils/translations.ts
+++ src/dynamic-checkout/utils/translations.ts
@@ -1,16 +1,16 @@
 /// <reference path="../references.ts" />
 
 module ProcessOut {
-  const locales = {
-    en: en,
-    es: es,
-    fr: fr,
-    pl: pl,
-    pt: pt,
-  };
-
   export class Translations {
     static getText(key: string, locale: string) {
+      const locales = {
+        en: en,
+        es: es,
+        fr: fr,
+        pl: pl,
+        pt: pt,
+      };
+
       const keys = locales[locale] || locales.en;
 
       return keys[key] || "";
