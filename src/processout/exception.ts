1b28696cc18a6aed537923a300a42be51bd0afd5
Jakub Jasiński <jakub.jasinski@checkout.com>
2026-03-13 09:49:55 +0100

    feat: add reason to payment cancelled for dc

--- src/processout/exception.ts
+++ src/processout/exception.ts
@@ -5,15 +5,20 @@
  */
 module ProcessOut {
 
+    export interface ExceptionMetadata {
+        [key: string]: any;
+    }
+
     /**
      * ProcessOut Exception class
      */
     export class Exception extends Error {
         public code:    string;
         public message: string;
+        public metadata: ExceptionMetadata;
         public stack:   string;
 
-        constructor(code: string, message?: string) {
+        constructor(code: string, message?: string, metadata?: ExceptionMetadata) {
             if (! message)
                 message = Translator.translateError(code);
 
@@ -21,6 +26,7 @@ module ProcessOut {
 
             this.code    = code;
             this.message = message;
+            this.metadata = metadata;
 
             this.name  = "ProcessOutException";
             this.stack = (<any> new Error()).stack;
